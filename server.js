b2839141832d8f8b5066e3eb82dcd067fdd76826
Angus Malloch <angusbass@hotmail.com>
2018-04-17 18:54:21 +0100

    Added a server.put method to update a coffee, also required in the ObjectID library in server.js. Tested using insomnia by put requesting a coffee changing only its type then using get all to find the same coffee had changed type to the put request.

--- server.js
+++ server.js
@@ -7,13 +7,14 @@ server.use(express.static('client/build'));
 server.use(parser.urlencoded({extended: true}));
 
 const MongoClient = require("mongodb").MongoClient;
+const ObjectID = require("mongodb").ObjectID;
 
 MongoClient.connect("mongodb://localhost:27017", function(err, client){
   if(err){
     console.log(err);
     return;
   }
-                  // database name
+  // database name
   const db = client.db("coffee");
 
   console.log("Connected to database");
@@ -42,7 +43,7 @@ MongoClient.connect("mongodb://localhost:27017", function(err, client){
   server.get("/api/coffees", function(req, res){
     const coffeesCollection = db.collection("coffees");
 
-                                // all coffees is what you find on the collection
+    // all coffees is what you find on the collection
     coffeesCollection.find().toArray(function(err, allCoffees){
       if(err){
         console.log(err);
@@ -55,22 +56,40 @@ MongoClient.connect("mongodb://localhost:27017", function(err, client){
   })
 
   server.delete("/api/coffees", function(req, res){
-  const coffeesCollection = db.collection("coffees");
-  // just an empty object
-  // tho it works without
-  const filterObject = {};
+    const coffeesCollection = db.collection("coffees");
+    // just an empty object
+    // tho it works without
+    const filterObject = {};
 
-  coffeesCollection.deleteMany(filterObject, function(err, result){
-    if(err){
-      console.log(err);
-      res.status(500);
+    coffeesCollection.deleteMany(filterObject, function(err, result){
+      if(err){
+        console.log(err);
+        res.status(500);
+        res.send();
+      }
+
+      res.status(204);
       res.send();
-    }
+    });
+  })
 
-    res.status(204);
-    res.send();
-  });
-})
+  server.put("/api/coffees/:id", function(req, res){
+    const coffeesCollection = db.collection("coffees");
+    // this coverts it into something mongo understands
+    const objectID = ObjectID(req.params.id);
+    const filterObject = { _id: objectID };
+    const updatedData = req.body;
+
+    coffeesCollection.update(filterObject, updatedData, function(err, result){
+      if(err){
+        console.log(err);
+        res.status(500);
+        res.send();
+      }
+      res.status(204);
+      res.send();
+    });
+  })
 
   server.listen(3000, function(){
     console.log("Listening on port 3000");
